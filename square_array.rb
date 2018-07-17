98a6ac6fcc164e19d20746c4c68830c83631aa60
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-17 19:33:06 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
-  array.each do |element|
-    new_array.push(element**2)
+  array.each do |number|
+    new_array.push(number**2)
   end
   return new_array
 end
